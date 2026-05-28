5b963b05f72016fdedfa9fb649654bc98d0ca737
yys806 <3492675568@qq.com>
2026-05-28 16:13:13 +0800

    Site updated: 2026-05-28 16:13:12

--- js/custom.js
+++ js/custom.js
@@ -203,7 +203,7 @@
       <div class="divider"></div>
       <div class="contact-info">
         <span>QQ: <a href="http://wpa.qq.com/msgrd?v=3&uin=3492675568&site=qq&menu=yes" target="_blank">3492675568</a></span>
-        <span>微信: yys08060910</span>
+        <span>微信: yu_yao_shen</span>
         <span>邮箱: <a href="mailto:yaoshen806@qq.com">yaoshen806@qq.com</a></span>
       </div>
       <div class="social-links">
